80469b8e477da8db4f2838a55828f0e6c50d8ead
fahim <basitabdullah1122@gmail.com>
2024-04-24 11:36:19 +0530

    Fixed

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,50 +1,83 @@
 import "./Video.scss";
 import { FaPlay } from "react-icons/fa";
-import { useEffect, useState } from "react";
 import { getDownloadURL, ref } from "firebase/storage";
 
 import { storage } from "../../firebase";
-import Loader from "../Loader/Loader";
 
 const Video = () => {
-  const [loading, setLoading] = useState(true);
-  const [videosArr, setVideosArr] = useState([]);
-  const [videoSrc, setVideoSrc] = useState(videosArr[0]);
   const vid1Ref = ref(storage, "/youtube-bluePrint/1 Welcome Video.mp4");
   const vid2Ref = ref(storage, "/youtube-bluePrint/2 Quit.mp4");
+  const vid3Ref = ref(storage, "/youtube-bluePrint/3 Harsh Truths.mp4");
+  const vid4Ref = ref(storage, "/youtube-bluePrint/4 Purpose.mp4");
+  const vid5Ref = ref(storage, "/youtube-bluePrint/5 Niche.mp4");
+  const vid6Ref = ref(storage, "/youtube-bluePrint/6 Hook.mp4");
+  const vid7Ref = ref(storage, "/youtube-bluePrint/7 Script.mp4");
+  const vid8Ref = ref(storage, "/youtube-bluePrint/8 Topics.mp4");
 
+  const btnData = [
+    {
+      name: "Welcome",
+      ref: vid1Ref,
+      id: "jddhvjksd",
+    },
+    {
+      name: "Quit",
+      ref: vid2Ref,
+      id: "dhcgsdds",
+    },
+    {
+      name: "Harsh Truths",
+      ref: vid3Ref,
+      id: "dhcgsvdfdds",
+    },
+    {
+      name: "Purpose",
+      ref: vid4Ref,
+      id: "vdffvdfv",
+    },
+    {
+      name: "Niche",
+      ref: vid5Ref,
+      id: "brbrtbrbrbvdd",
+    },
+    {
+      name: "Hook",
+      ref: vid6Ref,
+      id: "mhjmygbfgb",
+    },
+    {
+      name: "Script",
+      ref: vid7Ref,
+      id: "vfbgbrtbtrb",
+    },
+    {
+      name: "Topics",
+      ref: vid8Ref,
+      id: "dhcgsbrgbrtdds",
+    },
+  ];
+
+  const handleVideoSelection = (url) => {
+    const video = document.getElementById("video");
+    video.src = url;
+    video.load();
+    video.play();
+  };
   const fetchVids = (vidRef) => {
     getDownloadURL(vidRef)
       .then((url) => {
-        setVideosArr((prev) => [...prev, url]);
-        setLoading(false);
+        handleVideoSelection(url);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  useEffect(() => {
-    fetchVids(vid2Ref);
-    fetchVids(vid1Ref);
-  }, []);
-
-  const handleVideoSelection = (url) => {
-    setVideoSrc(url);
-    const video = document.getElementById("video");
-    video.src = url;
-    video.load();
-    video.play();
-  };
-
-  return loading ? (
-    <Loader/>
-  ) : (
+  return (
     <div className="video">
       <div className="videoWrapper">
         <video
           id="video"
-          src={videoSrc}
           controls
           controlsList="nodownload"
           autoPlay={true}
@@ -58,18 +91,12 @@ const Video = () => {
       </div>
       <div className="btns">
         <span>To Play Press the Buttons</span>
-        {videosArr.map((item, index) => {
-          return (
-            <button
-              className="btn"
-              onClick={() => handleVideoSelection(item)}
-              key={index + 1}
-            >
-              <FaPlay className="icon" />
-              <span>Lecture {index + 1}</span>
-            </button>
-          );
-        })}
+        {btnData.map((item) => (
+          <button className="btn" onClick={() => fetchVids(item.ref)} key={item.id}>
+            <FaPlay className="icon" />
+            <span>{item.name}</span>
+          </button>
+        ))}
       </div>
     </div>
   );
